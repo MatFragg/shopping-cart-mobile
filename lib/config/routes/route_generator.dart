73acf1f0ab93aae56f094a3db8a96b9bfac61d95
MatFragg <u202318323@upc.edu.pe>
2025-12-27 19:53:59 -0500

    refactor(products): add route generators for products routes

--- lib/config/routes/route_generator.dart
+++ lib/config/routes/route_generator.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_cart/features/authentication/presentation/pages/home_page.dart';
 import 'package:shopping_cart/features/authentication/presentation/pages/splash_page.dart';
+import 'package:shopping_cart/features/products/presentation/pages/browse_products_page.dart';
+import 'package:shopping_cart/features/products/presentation/pages/create_product_page.dart';
+import 'package:shopping_cart/features/products/presentation/pages/my_products_page.dart';
+import 'package:shopping_cart/features/products/presentation/pages/product_detail_page.dart';
 import '../../features/authentication/presentation/pages/login_page.dart';
 import '../../features/authentication/presentation/pages/register_page.dart';
+import '../../features/products/domain/entities/product.dart';
 import 'app_routes.dart';
-
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -20,6 +24,27 @@ class RouteGenerator {
 
       case AppRoutes.splash:
         return MaterialPageRoute(builder: (_) => const SplashPage());
+
+      case AppRoutes.browseProducts:
+        return MaterialPageRoute(builder: (_) => const BrowseProductsPage());
+
+      case AppRoutes.myProducts:
+        return MaterialPageRoute(builder: (_) => const MyProductsPage());
+
+      case AppRoutes.createProduct:
+        return MaterialPageRoute(builder: (_) => const CreateProductPage());
+
+      case AppRoutes.editProduct:
+        final product = settings.arguments as Product?;
+        return MaterialPageRoute(
+          builder: (_) => CreateProductPage(product: product),
+        );
+
+      case AppRoutes.productDetail:
+        final product = settings.arguments as Product;
+        return MaterialPageRoute(
+          builder: (_) => ProductDetailPage(product: product),
+        );
 
       default:
         return MaterialPageRoute(
