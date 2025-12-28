64125d8da144cc23598dffdcc1930f9a42e60866
MatFragg <u202318323@upc.edu.pe>
2025-12-27 19:53:13 -0500

    feat(products): add di declarations.

--- lib/config/injection_container.dart
+++ lib/config/injection_container.dart
@@ -1,12 +1,29 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:shopping_cart/core/constants/api_constants.dart';
+import 'package:shopping_cart/features/products/data/datasources/product_local_data_source.dart';
+import 'package:shopping_cart/features/products/data/datasources/product_remote_data_source.dart';
+import 'package:shopping_cart/features/products/data/repositories/product_repository_impl.dart';
+import 'package:shopping_cart/features/products/domain/repositories/product_repository.dart';
+import 'package:shopping_cart/features/products/domain/usecases/create_product.dart';
+import 'package:shopping_cart/features/products/domain/usecases/delete_product.dart';
+import 'package:shopping_cart/features/products/domain/usecases/get_all_products.dart';
+import 'package:shopping_cart/features/products/domain/usecases/get_my_products.dart';
+import 'package:shopping_cart/features/products/domain/usecases/get_products_by_category.dart';
+import 'package:shopping_cart/features/products/domain/usecases/search_products.dart';
+import 'package:shopping_cart/features/products/domain/usecases/update_product.dart';
+import 'package:shopping_cart/features/products/presentation/bloc/product_bloc.dart';
 import '../core/database/app_database.dart';
 import '../core/network/network_info.dart';
 import '../features/authentication/data/datasources/auth_local_data_source.dart';
@@ -19,6 +36,7 @@ import '../features/authentication/domain/usecases/login_user.dart';
 import '../features/authentication/domain/usecases/logout_user.dart';
 import '../features/authentication/domain/usecases/register_user.dart';
 import '../features/authentication/presentation/bloc/auth_bloc.dart';
+import 'package:path/path.dart' as p;
 
 final sl = GetIt.instance;
 
@@ -71,6 +89,22 @@ Future<void> init() async {
     }
   });
 
+  //! External
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+  sl.registerLazySingletonAsync<AppDatabase>(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'shopping_cart.db'));
+    debugPrint('DB path: ${file.path}');
+    final db = AppDatabase();
+    await db.customSelect('SELECT 1').get(); // Prueba de conexión
+    return db;
+  });
+  await sl.isReady<AppDatabase>();
+  sl.registerLazySingleton(() => const FlutterSecureStorage());
+  await sl.isReady<AppDatabase>();
+
+
   //! Features - Authentication
 
   // Bloc
@@ -113,15 +147,57 @@ Future<void> init() async {
         () => TokenDataSourceImpl(secureStorage: sl()),
   );
 
+  //! Features - Products
+  // Bloc
+  sl.registerFactory(
+        () => ProductBloc(
+      getAllProducts: sl(),
+      getMyProducts: sl(),
+      createProduct: sl(),
+      updateProduct: sl(),
+      deleteProduct: sl(),
+      searchProducts: sl(),
+      tokenDataSource:  sl(),
+      getProductsByCategory: sl(),
+    ),
+  );
+
+  // Use cases
+  sl.registerLazySingleton(() => GetAllProducts(sl()));
+  sl.registerLazySingleton(() => GetMyProducts(sl()));
+  sl.registerLazySingleton(() => CreateProduct(sl()));
+  sl.registerLazySingleton(() => UpdateProduct(sl()));
+  sl.registerLazySingleton(() => DeleteProduct(sl()));
+  sl.registerLazySingleton(() => SearchProducts(sl()));
+  sl.registerLazySingleton(() => GetProductsByCategory(sl()));
+
+
+
+  // Repository
+  sl.registerLazySingleton<ProductRepository>(
+        () => ProductRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      tokenDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  // Data sources
+  sl.registerLazySingleton<ProductRemoteDataSource>(
+        () => ProductRemoteDataSourceImpl(dio: sl()),
+  );
+
+  sl.registerLazySingleton<ProductLocalDataSource>(
+        () => ProductLocalDataSourceImpl(database: sl()),
+  );
+
   //! Core
   sl.registerLazySingleton<NetworkInfo>(
         () => NetworkInfoImpl(sl()),
   );
 
-  //! External
-  sl.registerLazySingleton(() => http.Client());
-  sl.registerLazySingleton(() => InternetConnectionChecker());
-  sl.registerLazySingleton(() => AppDatabase());
-  sl.registerLazySingleton(() => const FlutterSecureStorage());
+
+
 
 }
